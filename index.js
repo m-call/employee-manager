7a72d3d990fa23e6a91a0b7c9c203dd2dbbd1ccb
Michael Callahan <mcallahan0@yahoo.com>
2021-07-18 17:00:36 -0400

    Added more choices to the initPrompts. Added a depPrompts, manPrompts, addEmpPrompts, and removeEmpPrompts. Added base functionality for many of the functions that are triggered by the initPrompts user choice. Added blank functions for the newly created initPrompts choices

--- index.js
+++ index.js
@@ -34,7 +34,59 @@ const initPrompts = [
         type: 'list',
         message: 'What would you like to do?',
         name: 'startChoice',
-        choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager']
+        choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'Add Department', 'Remove Department', 'Quit']
+    }
+];
+
+const depPrompts = [
+    {
+        type: 'list',
+        message: 'Which department would you like to see employees for?',
+        name: 'department',
+        choices: ['Sales', 'Finance', 'Legal', 'Engineering']
+    }
+];
+
+const manPrompts = [
+    {
+        type: 'list',
+        message: 'Which manager do you want to see the employee list for?',
+        name: 'manager',
+        choices: ['Jeff Harkens', 'Aaron Loom', 'David Ernest', 'Michael Thomas']
+    }
+];
+
+const addEmpPrompts = [
+    {
+        type: 'input',
+        message: 'What is the employee\'s first name?',
+        name: 'firstName'
+    },
+    {
+        type: 'input',
+        message: 'What is the employee\'s last name?',
+        name: 'lastName'
+    },
+    {
+        type: 'list',
+        message: 'What is the employee\'s role?',
+        name: 'role',
+        choices: ['Salesperson', 'Sales Manager', 'Accountant', 'Account Manager', 'Lawyer', 'Legal Team Lead', 'Software Engineer', 'Lead Engineer']
+    },
+    {
+        type: 'list',
+        message: 'Who is the employee\'s manager?',
+        name: 'manager',
+        choices: ['None', 'Jeff Harkens', 'Aaron Loom', 'David Ernest', 'Michael Thomas']
+    }
+];
+
+const removeEmpPrompts = [
+    {
+        type: 'list',
+        message: 'Which employee do you want to remove?',
+        name: 'employee',
+        choices: []
     }
 ];
 
@@ -54,8 +106,15 @@ function init() {
                 removeEmployee();
             } else if (res.startChoice == 'Update Employee Role') {
                 updateRole();
-            } else if (res.startChoice == 'Update Employee Manager'){
+            } else if (res.startChoice == 'Update Employee Manager') {
                 updateManager();
+            } else if (res.startChoice == 'Add Department') {
+                addDepartment();
+            } else if (res.startChoice == 'Remove Department') {
+                removeDepartment();
+            } else if (res.startChoice == 'Quit') {
+                console.log('Thanks for using the Employee Manager!');
+                connection.end();
             } else {
                 connection.end();
             }
@@ -64,26 +123,48 @@ function init() {
 };
 
 function viewAllEmployees() {
+
     connection.query('SELECT * FROM employee', (err, results) => {
         if (err) throw err;
 
         console.table(results);
     });
+
 };
 
 function viewByDepartment() {
+
+    inquirer.prompt(depPrompts)
+        .then((res) => {
+
+        });
 
 };
 
 function viewByManager() {
 
+    inquirer.prompt(manPrompts)
+        .then((res) => {
+
+        });
+
 };
 
 function addEmployee() {
 
+    inquirer.prompt(addEmpPrompts)
+        .then((res) => {
+
+        });
+
 };
 
 function removeEmployee () {
+
+    inquirer.prompt(removeEmpPrompts)
+        .then((res) => {
+
+        });
 
 };
 
@@ -92,6 +173,14 @@ function updateRole() {
 };
 
 function updateManager() {
+
+};
+
+function addDepartment() {
+
+};
+
+function removeDepartment() {
 
 };
 
